b41287a432401a4c432b749a93c49096ca2a3d00
DEKAN\onrio <lukaszdekanski@gmail.com>
2023-01-15 22:02:44 +0100

    controls have been adjusted

--- SigmaApp/SigmaApp/Controls/LoginTextBox.xaml.cs
+++ SigmaApp/SigmaApp/Controls/LoginTextBox.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace SigmaApp
 {
@@ -11,5 +13,51 @@ namespace SigmaApp
         {
             InitializeComponent();
         }
+
+
+
+        public String PlaceHolder
+        {
+            get { return (String)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(String), typeof(LoginTextBox));
+
+
+        public String Text
+        {
+            get { return (String)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(LoginTextBox));
+
+
+
+
+        public bool IsPasswoed
+        {
+            get { return (bool)GetValue(IsPasswoedProperty); }
+            set { SetValue(IsPasswoedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPasswoed.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswoedProperty =
+            DependencyProperty.Register("IsPasswoed", typeof(bool), typeof(LoginTextBox));
+
+
+        private void passbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            login.Text = passbox.Password;
+        }
+
+
+
+
     }
 }
