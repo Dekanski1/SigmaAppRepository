f2830f039b97ab77e566507e1e14ecceb067ca68
DEKAN\onrio <lukaszdekanski@gmail.com>
2023-01-15 14:50:46 +0100

    Created first Page, Style and  Controls

---
+++ SigmaApp/SigmaApp/Controls/LoginTextBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SigmaApp
+{
+    /// <summary>
+    /// Interaction logic for LoginTextBox.xaml
+    /// </summary>
+    public partial class LoginTextBox : UserControl
+    {
+        public LoginTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- SigmaApp/SigmaApp/MainWindow.xaml.cs
+++ SigmaApp/SigmaApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SigmaApp
 {
@@ -23,6 +11,9 @@ namespace SigmaApp
         public MainWindow()
         {
             InitializeComponent();
+
+            Main.Navigate(new Uri("/Pages/LoginPage.xaml", UriKind.RelativeOrAbsolute));
         }
+        
     }
 }
